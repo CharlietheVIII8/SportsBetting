ef9aec78d41677964bd8a2ad1b1b39f9e10baa93
CharlietheVIII8 <61073757+CharlietheVIII8@users.noreply.github.com>
2021-01-12 13:09:43 -0500

    Initial Commit

--- SportsBetting/asgi.py
+++ SportsBetting/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for untitled2 project.
+ASGI config for SportsBetting project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SportsBetting.settings')
 
 application = get_asgi_application()
