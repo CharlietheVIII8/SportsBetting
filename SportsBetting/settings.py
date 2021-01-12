cd18c8ae88b3243680b80f4b7b6a6ff79cb28409
CharlietheVIII8 <61073757+CharlietheVIII8@users.noreply.github.com>
2021-01-12 14:03:48 -0500

    NCAAB

--- SportsBetting/settings.py
+++ SportsBetting/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'untitled2.urls'
+ROOT_URLCONF = 'SportsBetting.urls'
 
 TEMPLATES = [
     {
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'SportsBetting.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SportsBetting',
+        'USER': 'postgres',
+        'PASSWORD': '1228gizmo',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
