5f87554fcbd46b7f22c37c9d74345d0739e01c40
Charlie Mitchell <charliem@kaitongo.com>
2021-01-19 00:10:23 -0500

    box scores

--- NCAAB/admin.py
+++ NCAAB/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Team, Player, PlayerBoxscore, GameBoxscore
+from .models import Team, PlayerBoxscore, GameBoxscore
 
 
 # Register your models here.
@@ -9,17 +9,14 @@ class TeamAdmin(admin.ModelAdmin):
     list_display = ("abbreviation", "name", "conference")
     search_fields = ("abbreviation", "name", "conference")
 
-@admin.register(Player)
-class PlayerAdmin(admin.ModelAdmin):
-    list_display = ("name", "player_id")
-    search_fields = ("name", "player_id")
 
 @admin.register(PlayerBoxscore)
 class PlayerBoxscoreAdmin(admin.ModelAdmin):
     list_display = ("player",)
     search_fields = ("player",)
 
+
 @admin.register(GameBoxscore)
 class GameBoxscoreAdmin(admin.ModelAdmin):
-    list_display = ("winning_name", "losing_name", "date")
-    search_fields = ("winning_name", "losing_name", "date")
+    list_display = ("winner", "loser", "date")
+    search_fields = ("winner", "loser", "date")
